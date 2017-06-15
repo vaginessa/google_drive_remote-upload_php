7c6f099030a43af88ada77a980158e65d5eb7187
sachin36987 <sachin36987@gmail.com>
2017-06-15 18:44:47 +0530

    Update change_account.php

--- Google Drive Remote Upload PHP Script/change_account.php
+++ Google Drive Remote Upload PHP Script/change_account.php
@@ -1,18 +1,17 @@
+
 <?php
 /**********************************
-// Your are free to modify and distribute it as long as below three lines are there 
-//in the file. 
-//@author Sachin Khokhar
-//@website http://savedrive.faltutech.club
 
+// Your are free to modify and distribute it as long as below three lines are there
+// in the file.
+// @author Sachin Khokhar
+// @website http://savedrive.faltutech.club
 
-    logout page
+logout page
 *****************************************/
-
 session_start();
 session_unset();
 session_destroy();
-
-$url2='/authorize.php';
+$url2 = '/authorize.php';
 header('Location: ' . filter_var($url2, FILTER_SANITIZE_URL));
 ?>
